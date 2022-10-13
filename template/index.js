363228a056b1757f84735afbe70caa2b80fffe71
opect.pol@i.ua <opect.pol@i.ua>
2022-10-13 17:29:21 +0300

    add templates

---
+++ template/index.js
@@ -0,0 +1,11 @@
+// import "normalize.css"
+import "./modals/index.styl"
+import "./style/index.scss"
+import "./style/index.styl"
+import "./style/index.less"
+
+const main = () => {
+  //
+}
+
+main()
